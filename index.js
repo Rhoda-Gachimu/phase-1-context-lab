c00a1bf4078d24b4f2a00de6bc070055e89bc5d3
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-29 21:04:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(arrOfArrays) {
+    const EmployeeRecords = []
+    arrOfArrays.map(array => EmployeeRecords.push(createEmployeeRecord(array)))
+    return EmployeeRecords
+}
+function createTimeInEvent(date) {
+    const timeInObj = {
+        type: "TimeIn",
+        hour: parseInt(date.split(" ")[1], 10),
+        date: date.split(" ")[0]
+    }
+    this.timeInEvents.push(timeInObj)
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    const timeoutObj = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1], 10),
+        date: dateStamp.split(" ")[0]
+    }
+    this.timeOutEvents.push(timeoutObj)
+    return this
+}
+function hoursWorkedOnDate(date) {
+    const timeinObj = this.timeInEvents.find(dayObj => dayObj.date === date)
+    const timeinHours = timeinObj.hour
+    const timeoutObj = this.timeOutEvents.find(dayObj => dayObj.date === date)
+    const timeoutHours = timeoutObj.hour
+    const time = timeoutHours - timeinHours
+    const hours = Math.floor(time/100)
+    return hours
+}
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    const payPerHour = this.payPerHour
+    return hoursWorked * payPerHour
+}
+/*
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +69,17 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(obj => obj.firstName === firstName)
+}
+
+
+function calculatePayroll(arrayOfEmpRecords) {
+    let totalPay = 0
+    arrayOfEmpRecords.forEach(obj => {
+        totalPay += allWagesFor.call(obj)
+    })
+    return totalPay
+}
